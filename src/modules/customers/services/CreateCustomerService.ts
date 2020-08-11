35a352533c9cdb7691c76cb9f52ad81b39959f82
Levi Siebra <levils114@gmail.com>
2020-08-11 03:15:33 -0300

    finish challenge

---
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -0,0 +1,31 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import Customer from '../infra/typeorm/entities/Customer';
+import ICustomersRepository from '../repositories/ICustomersRepository';
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+@injectable()
+class CreateCustomerService {
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) { }
+
+  public async execute({ name, email }: IRequest): Promise<Customer> {
+    const customer = await this.customersRepository.findByEmail(email);
+
+    if (customer) throw new AppError('User email already exists', 400);
+
+    const newCustomer = await this.customersRepository.create({ name, email });
+
+    return newCustomer;
+  }
+}
+
+export default CreateCustomerService;
